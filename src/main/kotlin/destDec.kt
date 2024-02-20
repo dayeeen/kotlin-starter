47833445dc69bc6c9ff44e467a6ee232175240dd
dayeeen <dayensptr@gmail.com>
2024-02-20 23:04:26 +0700

    adding destructure declarations

---
+++ src/main/kotlin/destDec.kt
@@ -0,0 +1,13 @@
+// Destructuring Declarations
+
+fun main(){
+    val dataUser = DataUser("Dayen", 17)
+
+//    val name = dataUser.component1()
+//    val age = dataUser.component2()
+
+    // Atau bisa disingkat menjadi seperti berikut:
+    val (name, age) = dataUser
+
+    println("My name is $name, I am $age years old")
+}
